1eede11c76d431a1a32e9609f3aa1af2e53a70a9
Ralf Waspe <rwaspe@icloud.com>
2025-07-24 14:14:46 +0200

    fixed clear

--- lib/bmk/manager_clear.py
+++ lib/bmk/manager_clear.py
@@ -18,21 +18,17 @@ class ManagerClear(ManagerAbstract):
          print(Console.magenta('no tag given'))
          return
 
-      inDir = self.tag in self.data[ManagerAbstract.DirKey]
-      inRepo = self.tag in self.data[ManagerAbstract.RepoKey]
-      if not inDir and not inRepo:
+      found = False
+      for sectionName in [ManagerAbstract.DirKey, ManagerAbstract.RepoKey, ManagerAbstract.SyncKey]:
+         if not self.tag in self.data[sectionName]:
+            continue
+
+         del self.data[sectionName][self.tag]
+         found = True
+         print(Console.green('removed tag'), ':', self.tag, ' from section', sectionName)
+
+      if not found:
          print(Console.magenta('unkown tag'), ':', self.tag)
          return
 
-      if inDir:
-         self.clear(ManagerAbstract.DirKey, self.tag)
-      if inRepo:
-         self.clear(ManagerAbstract.RepoKey, self.tag)
-
       self.save()
-
-   def clear(self, sectionName, tag):
-
-      del self.data[sectionName][tag]
-
-      print(Console.green('removed tag'), ':', tag, ' from section', sectionName)
